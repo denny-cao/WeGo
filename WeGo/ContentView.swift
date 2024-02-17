e5bf0cadcc508626b311de770ff0e62dc0d8abe5
efrainangon-cruz <efrainac@stanford.edu>
2024-02-17 00:53:23 -0800

    Initial Commit

---
+++ WeGo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeGo
+//
+//  Created by Efrain Angon-Cruz on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeGo/WeGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeGoApp.swift
+//  WeGo
+//
+//  Created by Efrain Angon-Cruz on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
